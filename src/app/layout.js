92b5d0949ac7403ffb7df517a8ad5cfe0bb86704
Aayush  Karki <aayush@aiims.com.au>
2024-07-04 22:12:32 +1000

    html lang updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="au">
+    <html lang="en-AU">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/dns8eeo.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
